109e18144ff54038a0bb8f232365cd3e95d76301
Viacheslav Kulichenko <48729747+KulichenkoViacheslav@users.noreply.github.com>
2022-10-04 13:10:49 +0300

    Create button module

---
+++ Task_1/Components/Button/button.h
@@ -0,0 +1,12 @@
+/**
+  ******************************************************************************
+  * @file    button.h
+  * @brief   This file contains all the function prototypes for
+  *          the leds modules
+  ******************************************************************************
+  */
+  
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#endif /* BUTTON_H */
